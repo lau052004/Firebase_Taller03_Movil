4cb0b7a2ee05ee22faba4319f87494675b171751
Santicm23 <santicastrom@outlook.com>
2023-11-09 00:26:23 -0500

    menu navegación

--- app/src/main/java/com/lauovalle/taller_03_lauraovalle/MenuFragment.kt
+++ app/src/main/java/com/lauovalle/taller_03_lauraovalle/MenuFragment.kt
@@ -1,59 +1,65 @@
 package com.lauovalle.taller_03_lauraovalle
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lauovalle.taller_03_lauraovalle.databinding.FragmentMenuBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentMenuBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        binding = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mAuth = Firebase.auth
+
+        setup()
+    }
+
+    private fun setup() {
+        binding.menu.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item_home -> {
+                    if (requireActivity() is HomeActivity) {
+                        return@setOnItemSelectedListener true
+                    }
+                    val intent = Intent(requireContext(), HomeActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.item_update -> {
+                    true
+                }
+                R.id.item_available -> {
+                    true
+                }
+                R.id.item_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    requireActivity().finish()
+                    true
+                }
+
+                else -> {
+                    false
                 }
             }
+        }
     }
 }
